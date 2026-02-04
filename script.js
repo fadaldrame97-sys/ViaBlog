e867473ba2d28a6422c25685777ce2c589e773e7
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-02-04 11:01:43 +0100

    Tester les éléments dans le console

--- script.js
+++ script.js
@@ -15,12 +15,14 @@ const cat=document.getElementById("ctg");
 // };
 
 
-
+console.log(titr);
 
 const modal = document.getElementById("modal");
 const formDestination = document.getElementById("Destination_page");
 
 formDestination.addEventListener("submit", (event) => { event.preventDefault();
+
+   
 
 const voyage = {
   titre: document.getElementById("Titre").value,
@@ -37,5 +39,24 @@ voyages.push(voyage);
   
   formDestination.reset();
 
+  console.log(titr,destination);
+  console.log("form:", formDestination);
+
+
+    titr.textContent = voyage.titre;
+  destination.textContent = voyage.destination;
+  cat.textContent = voyage.categorie;
+  img.src = voyage.image;
+  img.alt = voyage.titre;
+
+  // Fermer le modal et reset
+  modal.classList.add("hidden");
+  formDestination.reset();
+
+  // 🔹 Vérification dans la console
+  console.log("voyages:", voyages);
+
+
 
 });
+
